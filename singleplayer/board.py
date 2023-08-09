285b8c499c9bb2b65414d1fcb3e8e8cdf2598dc2
Poggecci <alejandro.cruzado@upr.edu>
2023-08-08 23:15:21 -0700

    Update Singleplayer code with final remote version

--- singleplayer/board.py
+++ singleplayer/board.py
@@ -1,7 +1,8 @@
 from enums import Color
-from player_piece import PieceShard
+from player_piece import PlayerPiece, PieceShard
 from player import Player
 import numpy as np
+
 
 class GridItem:
 
@@ -27,14 +28,6 @@ class Board:
       ]) for row in self.grid_items
     ]))
 
-  '''Print method used when the terminal supports colors'''
-  def pretty_print(self):
-      print("\n".join([
-      " ".join([
-        "." if grid_item is None else grid_item.owner_color.value
-        for grid_item in row
-      ]) for row in self.grid_items
-    ]))
   def print_mock(self, piece_shards: list[PieceShard], color: Color):
     valid = self.validate(piece_shards, color)
     mock = [["." if y is None else y.owner_color.value for y in x]
@@ -52,27 +45,40 @@ class Board:
         return True
     return False
 
+  def out_of_bounds_offset(self, piece_shards: list[PieceShard]) -> tuple[int]:
+    offset_i, offset_j = 0, 0
+    for shard in piece_shards:
+      i, j = shard.position[0], shard.position[1]
+      if i < 0:
+        offset_i = min(i, offset_i)
+      elif i >= len(self.grid_items):
+        offset_i = max(i - len(self.grid_items) + 1, offset_i)
+      if j < 0:
+        offset_j = min(j, offset_j)
+      elif j >= len(self.grid_items):
+        offset_j = max(j - len(self.grid_items) + 1, offset_j)
+    return (offset_i, offset_j)
+
+  def position_out_of_bounds(self, i: int, j: int) -> bool:
+    return i < 0 or j < 0 or i >= len(self.grid_items) or j >= len(
+      self.grid_items[0])
+
+  def unit_is_color(self, i: int, j: int, color: Color):
+    return not self.position_out_of_bounds(i, j) and self.grid_items[i][
+      j] is not None and self.grid_items[i][j].owner_color == color
+
+  def first_move(self, color: Color) -> bool:
+    for row in self.grid_items:
+      for grid_item in row:
+        if grid_item is not None and grid_item.owner_color == color:
+          return False
+    return True
+
   def validate(self, piece_shards: list[PieceShard], color: Color) -> bool:
-
-    def position_out_of_bounds(i: int, j: int) -> bool:
-      return i < 0 or j < 0 or i >= len(self.grid_items) or j >= len(
-        self.grid_items[0])
-
-    def unit_is_color(i: int, j: int, color: Color):
-      return not position_out_of_bounds(i, j) and self.grid_items[i][
-        j] is not None and self.grid_items[i][j].owner_color == color
-
-    def first_move(color: Color) -> bool:
-      for row in self.grid_items:
-        for grid_item in row:
-          if grid_item is not None and grid_item.owner_color == color:
-            return False
-      return True
-
     if self.out_of_bounds(piece_shards):
       return False
 
-    is_first_move = first_move(color)
+    is_first_move = self.first_move(color)
     touches_corner = False
     is_corner = False
     touches_side = False
@@ -89,49 +95,62 @@ class Board:
                             or i == len(self.grid_items) - 1
                             and j == len(self.grid_items[0]) - 1):
         is_corner = True
-      if not is_first_move and (unit_is_color(i + 1, j, color)
-                                or unit_is_color(i - 1, j, color)
-                                or unit_is_color(i, j + 1, color)
-                                or unit_is_color(i, j - 1, color)):
+      if not is_first_move and (self.unit_is_color(i + 1, j, color)
+                                or self.unit_is_color(i - 1, j, color)
+                                or self.unit_is_color(i, j + 1, color)
+                                or self.unit_is_color(i, j - 1, color)):
         touches_side = True
         break
-      if not is_first_move and (unit_is_color(i + 1, j + 1, color)
-                                or unit_is_color(i - 1, j + 1, color)
-                                or unit_is_color(i + 1, j - 1, color)
-                                or unit_is_color(i - 1, j - 1, color)):
+      if not is_first_move and (self.unit_is_color(i + 1, j + 1, color)
+                                or self.unit_is_color(i - 1, j + 1, color)
+                                or self.unit_is_color(i + 1, j - 1, color)
+                                or self.unit_is_color(i - 1, j - 1, color)):
         touches_corner = True
 
     return (is_first_move and is_corner or not is_first_move and touches_corner
             and not touches_side) and not intersects
 
   def find_possible_corners(self, player: Player):
-    possible_corners =  np.random.rand(Board.grid_size, Board.grid_size) > 1
+    possible_corners = np.zeros([Board.grid_size, Board.grid_size])
     for row in range(Board.grid_size):
-        for col in range(Board.grid_size):
-            free_square =  self.grid_items[row][col] is None
-            same_color_corner = (self.unit_is_color(row + 1, col + 1, player.color)
-                or self.unit_is_color(row - 1, col + 1, player.color)
-                or self.unit_is_color(row + 1, col - 1, player.color)
-                or self.unit_is_color(row - 1, col - 1, player.color))
-            same_color_side = (self.unit_is_color(row + 1, col, player.color)
-                or self.unit_is_color(row - 1, col, player.color)
-                or self.unit_is_color(row, col + 1, player.color)
-                or self.unit_is_color(row, col - 1, player.color))
-            if (free_square and same_color_corner and not same_color_side):
-                for rOff in range(-2,3):
-                    for cOff in range(-2,3):
-                        possible_corners[row+rOff,col+cOff] = True
+      for col in range(Board.grid_size):
+        free_square = self.grid_items[row][col] is None
+        same_color_corner = (
+          self.unit_is_color(row + 1, col + 1, player.color)
+          or self.unit_is_color(row - 1, col + 1, player.color)
+          or self.unit_is_color(row + 1, col - 1, player.color)
+          or self.unit_is_color(row - 1, col - 1, player.color))
+        same_color_side = (self.unit_is_color(row + 1, col, player.color)
+                           or self.unit_is_color(row - 1, col, player.color)
+                           or self.unit_is_color(row, col + 1, player.color)
+                           or self.unit_is_color(row, col - 1, player.color))
+        if (free_square and same_color_corner and not same_color_side):
+          for rOff in range(-2, 3):
+            for cOff in range(-2, 3):
+              nRow = min(max(row + rOff, 0), Board.grid_size - 1)
+              nCol = min(max(col + cOff, 0), Board.grid_size - 1)
+              possible_corners[nRow, nCol] = 1
     return possible_corners
-                
-  def any_valid_move(self, player: Player):
-    possible_corners = self.find_possible_corners(player)
-    for piece in player.pieces:
-      for a in range(2):
-        for b in range(4):
-          for i in range(Board.grid_size):
-            for j in range(Board.grid_size):
-              if possible_corners[i,j] and self.validate(piece.split((i, j)), player.color):
-                return True
-          piece.rotate_90()
-        piece.flip_horizontal()
+
+  def any_valid_move_piece(self, piece: PlayerPiece, color: Color,
+                           possible_corners):
+    for a in range(2):
+      for b in range(4):
+        for i in range(Board.grid_size):
+          for j in range(Board.grid_size):
+            if possible_corners[i, j] == 1 and self.validate(
+                piece.split((i, j)), color):
+              return True
+        piece.rotate_90()
+      piece.flip_horizontal()
     return False
+
+  def any_valid_move(self, player: Player):
+    if self.first_move(player.color):
+      return [True for piece in player.pieces]
+
+    possible_corners = self.find_possible_corners(player)
+    return [
+      self.any_valid_move_piece(piece, player.color, possible_corners)
+      for piece in player.pieces
+    ]
--- singleplayer/main.py
+++ singleplayer/main.py
@@ -2,7 +2,8 @@ from enums import Color
 from player_piece import PlayerPiece
 from state import State
 from board import Board
-from getkey import getkey,key
+from piece import pieces
+from getkey import getkey, keys
 import os
 import math
 import time
@@ -15,6 +16,7 @@ state = State()
 page = "start"
 page_data = {}
 alerts = []
+valid_moves = [True for piece in pieces]
 
 
 def character_limiter(x, limit, append=""):
@@ -33,94 +35,139 @@ def character_limiter(x, limit, append=""):
   return x
 
 
-def get_printable_piece(piece: PlayerPiece, color: Color):
+def get_printable_piece(piece: PlayerPiece, color: Color, valid: bool):
   result = []
   for i in range(len(piece.shape)):
-    result.append(" ".join(
-      [" " if y == 0 else color.value for y in piece.shape[i]]))
+    result.append(" ".join([
+      " " if y == 0 else color.value if valid else "X" for y in piece.shape[i]
+    ]))
   return result
 
 
-def print_target_piece(piece: PlayerPiece, color: Color):
-  result = "      |    \n"
-  for i in range(len(piece.shape)):
-    result += ("--" if i == 2 else "  ") + " ".join(
-      [" " if y == 0 else color.value
-       for y in piece.shape[i]]) + ("--" if i == 2 else "") + "\n"
-  result += "      |    "
-  print(result)
+def handle_goto_flags(flags):
+  if "any_valid_moves" in flags:
+    global valid_moves
+    player_index = state.turn % len(state.players)
+    if state.pass_tracker[player_index]:
+      valid_moves = [False for piece in state.players[player_index].pieces]
+      state.pass_turn()
+      goto("pieces", {"index": 0, "num_buffer": ""}, ["any_valid_moves"])
+      raise_alert("No valid moves so turn was skipped")
+    valid_moves = state.board.any_valid_move(state.players[player_index])
+    if not any(valid_moves):
+      state.pass_turn()
+      goto("pieces", {"index": 0, "num_buffer": ""}, ["any_valid_moves"])
+      raise_alert("No valid moves so turn was skipped")
 
 
-def parse_position(position_code):
-  if len(position_code) != 2:
-    return (-1, -1)
-  return (ord(position_code[0].upper()) - ord("A"),
-          ord(position_code[1].upper()) - ord("A"))
-
-
-def goto(x: int, data: dict = None):
+def goto(x: int, data: dict = None, flags=None):
   global page, page_data
   if data is None:
     data = {}
+  if flags is None:
+    flags = []
   page = x
   page_data = data
+  handle_goto_flags(flags)
 
 
 def display_page():
+  global valid_moves
   player = state.players[state.turn % len(state.players)]
   if page == "start":
     print(
       character_limiter(
         "Welcome to Blokus!\n\nPlace your tetrominoes on the board, corner-to-corner, blocking your opponents. Maximize territory to win!\n\nPress enter to start the game.",
         character_limit))
+
   elif page == "board":
     print(f"Turn #{state.turn+1}: {player.name}'s Turn")
     state.board.print()
+
   elif page == "pieces":
     print(f"Turn #{state.turn+1}: {player.name}'s Turn")
     printable_pieces = []
     for i in range(len(player.pieces)):
-      piece_num_string = f"Pc #{i+1}"
-      printable_piece = get_printable_piece(player.pieces[i], player.color)
+      piece_num_string = f"Piece #{i+1}"
+      printable_piece = get_printable_piece(player.pieces[i], player.color,
+                                            valid_moves[i])
       piece_num_string += " " * (len(printable_piece[0]) -
                                  len(piece_num_string))
-      if i == page_data['index']:
-        piece_num_string = piece_num_string[:-1] + "X"
       printable_pieces.append([piece_num_string] + printable_piece)
-    width = 5  #math.ceil(math.sqrt(len(player.pieces)))
+    width = math.ceil(math.sqrt(len(player.pieces)))
     s = ""
     for i in range(0, len(printable_pieces), width):
+      for n in range(width):
+        s += (("+" + "-" * len(printable_pieces[0][0]) +
+               "+") if i + n == page_data['index']
+              or i + n == page_data['index'] + width else " " *
+              (len(printable_pieces[0][0]) + 2))
+      s += "\n"
       for j in range(len(printable_pieces[0])):
         for n in range(width):
           if i + n < len(printable_pieces):
-            s += printable_pieces[i + n][j] + "  "
+            separator = ("|" if i + n == page_data['index'] else " ")
+            s += separator + printable_pieces[i + n][j] + separator
+        s += "\n"
+      if i < len(printable_pieces) <= i + width:
+        for n in range(width):
+          s += (("+" + "-" * len(printable_pieces[0][0]) +
+                 "+") if i + n == page_data['index'] else " " *
+                (len(printable_pieces[0][0]) + 2))
         s += "\n"
     print(s)
     if page_data["num_buffer"] != "":
-      print(f"Buffer... {page_data['num_buffer']}")
+      print(f"Selecting... {page_data['num_buffer']}")
+
   elif page == "piece":
     piece = player.pieces[page_data["index"]]
     print(f"Turn #{state.turn+1}: {player.name}'s Turn")
     state.board.print_mock(piece.split(page_data["position"]), player.color)
+
   elif page == "place_piece":
     print(f"Turn #{state.turn+1}: {player.name}'s Turn")
     state.board.print_mock(
       player.pieces[page_data["index"]].split(page_data["position"]),
       player.color)
     print("Confirm piece position? (Y/n) ")
+
+  elif page == "menu":
+    s = "Menu:\n"
+    for i, action in enumerate(
+      ["Continue", "View Instructions", "View Help", "Exit Game"]):
+      indicator = ">" if page_data["select"] == i else " "
+      s += indicator + " " + action + "\n"
+    print(s)
+
   elif page == "help":
-    print(
-      character_limiter(
-        "Help:\n"
-        " -instructions: Show the game's instructions\n"
-        " -board: Show the game board\n"
-        " -pieces: Show your current pieces\n"
-        " -piece [x]: Focus on a specific piece\n"
-        " -rotate [+/-]+: Rotate the focused piece\n"
-        " -flip [h/v]: Flip the focused piece\n"
-        " -place [RowCol]: Place the focused piece\n"
-        " -pass: Pass your turn onto the next player\n"
-        " -exit: Exit the game\n", character_limit, "  "))
+    target = page_data["page"]
+    print("Help:")
+    if target == "pieces":
+      print(
+        character_limiter(
+          " -Press WASD to move the cursor\n"
+          " -Press B to view the board\n"
+          " -Press any number to select piece by number\n"
+          " -Press Enter to select the piece\n", character_limit, "  "))
+    elif target == "board":
+      print(
+        character_limiter(" -Press B to go back to your pieces\n",
+                          character_limit, "  "))
+    elif target == "piece":
+      print(
+        character_limiter(
+          " -Press WASD to move the piece\n"
+          " -Press Q/E to rotate the piece\n"
+          " -Press Z to flip the piece vertically\n"
+          " -Press X to flip the piece horizontally\n"
+          " -Press P to view your pieces\n"
+          " -Press < or > to swap the current piece\n"
+          " -Press 1, 2, 3, 4 to position the piece in a quadrant\n"
+          " -Press 0 to reset the piece's position\n"
+          " -Press Enter to place the piece\n", character_limit, "  "))
+    else:
+      pass
+
   elif page == "instructions":
     print(
       character_limiter(
@@ -152,81 +199,85 @@ def update_screen():
 update_screen()
 while True:
   try:
+    winner = state.check_win()
+    if winner is not None:
+      os.system(clear)
+      print(f"{winner.name} has won!")
+      state.board.print()
+      if input("Would you like to play again? (Y/n) ").upper() == "Y":
+        state = State()
+        page = "board"
+        page_data = {}
+        alerts = []
+        continue
+      else:
+        break
+
     player = state.players[state.turn % len(state.players)]
-
-    if state.pass_tracker[state.turn % len(
-        state.players)] or not state.board.any_valid_move(player):
-      state.pass_turn()
-      goto("pieces", {"index": 0, "num_buffer": ""})
-      raise_alert("You have no valid moves")
-      update_screen()
-      continue
-
     key = getkey()
 
     if page == "start":
       if key == keys.ENTER:
         goto("pieces", {"index": 0, "num_buffer": ""})
         update_screen()
+
     elif page == "pieces":
+      width = math.ceil(math.sqrt(len(player.pieces)))
       if key == "w":
-        if page_data.get("index") > 5:
-          page_data["index"] -= 5
+        if page_data.get("index") > width:
+          page_data["index"] -= width
         else:
           page_data["index"] = 0
         page_data["num_buffer"] = ""
-        update_screen()
       elif key == "s":
-        if page_data.get("index") < len(player.pieces) - 5:
-          page_data["index"] += 5
+        if page_data.get("index") < len(player.pieces) - width:
+          page_data["index"] += width
         else:
           page_data["index"] = len(player.pieces) - 1
         page_data["num_buffer"] = ""
-        update_screen()
       elif key == "a":
         if page_data.get("index") > 0:
           page_data["index"] -= 1
         else:
           page_data["index"] = 0
         page_data["num_buffer"] = ""
-        update_screen()
       elif key == "d":
         if page_data.get("index") < len(player.pieces) - 1:
           page_data["index"] += 1
         else:
           page_data["index"] = len(player.pieces) - 1
         page_data["num_buffer"] = ""
-        update_screen()
       elif key.isdecimal():
         buffer = int(page_data["num_buffer"] + key)
         if 0 < buffer <= len(player.pieces):
           page_data["num_buffer"] += key
           page_data["index"] = buffer - 1
-          update_screen()
       elif key == keys.BACKSPACE:
         if page_data["num_buffer"] != "":
           page_data["num_buffer"] = page_data["num_buffer"][:-1]
           if page_data["num_buffer"] != "":
             page_data["index"] = int(page_data["num_buffer"]) - 1
-          else:
-            page_data["index"] = 0
-          update_screen()
       elif key == keys.ENTER:
-        goto(
-          "piece", {
-            "index": page_data["index"],
-            "position": (Board.grid_size // 2, Board.grid_size // 2)
-          })
-        update_screen()
+        if valid_moves[page_data["index"]]:
+          goto(
+            "piece", {
+              "index": page_data["index"],
+              "position": (Board.grid_size // 2, Board.grid_size // 2)
+            })
+        else:
+          raise_alert("No valid moves for this piece")
       elif key == "b":
         goto(
           "board", {
             "index": page_data.get("index"),
             "num_buffer": page_data.get("num_buffer")
           })
+      elif key == keys.ESCAPE:
+        goto("menu", {"page": page, "data": page_data, "select": 0})
       else:
         continue
       update_screen()
+
     elif page == "board":
       if key == "b":
         goto(
@@ -234,9 +285,12 @@ while True:
             "index": page_data.get("index"),
             "num_buffer": page_data.get("num_buffer")
           })
+      elif key == keys.ESCAPE:
+        goto("menu", {"page": page, "data": page_data, "select": 0})
       else:
         continue
       update_screen()
+
     elif page == "piece":
       piece = player.pieces[page_data["index"]]
       i, j = page_data["position"]
@@ -252,103 +306,113 @@ while True:
       elif key == "d":
         if not state.board.out_of_bounds(piece.split((i, j + 1))):
           page_data["position"] = (i, j + 1)
+      elif key == "0":
+        page_data["position"] = (Board.grid_size // 2, Board.grid_size // 2)
+      elif key in ["1", "2", "3", "4"]:
+        positions = [(Board.grid_size // 4, Board.grid_size // 4),
+                     (Board.grid_size // 4, Board.grid_size * 3 // 4),
+                     (Board.grid_size * 3 // 4, Board.grid_size // 4),
+                     (Board.grid_size * 3 // 4, Board.grid_size * 3 // 4)]
+        page_data["position"] = positions[int(key) - 1]
       elif key == "q":
         piece.rotate_neg_90()
+        offset = state.board.out_of_bounds_offset(
+          piece.split(page_data["position"]))
+        page_data["position"] = (page_data["position"][0] - offset[0],
+                                 page_data["position"][1] - offset[1])
       elif key == "e":
         piece.rotate_90()
+        offset = state.board.out_of_bounds_offset(
+          piece.split(page_data["position"]))
+        page_data["position"] = (page_data["position"][0] - offset[0],
+                                 page_data["position"][1] - offset[1])
       elif key == "x":
         piece.flip_horizontal()
+        offset = state.board.out_of_bounds_offset(
+          piece.split(page_data["position"]))
+        page_data["position"] = (page_data["position"][0] - offset[0],
+                                 page_data["position"][1] - offset[1])
       elif key == "z":
         piece.flip_vertical()
+        offset = state.board.out_of_bounds_offset(
+          piece.split(page_data["position"]))
+        page_data["position"] = (page_data["position"][0] - offset[0],
+                                 page_data["position"][1] - offset[1])
       elif key == "p":
         goto("pieces", {"index": page_data.get("index"), "num_buffer": ""})
       elif key == keys.LEFT:
-        if page_data["index"] > 0:
-          page_data["index"] -= 1
-        else:
-          page_data["index"] = len(player.pieces) - 1
+        while True:
+          if page_data["index"] > 0:
+            page_data["index"] -= 1
+          else:
+            page_data["index"] = len(player.pieces) - 1
+          if valid_moves[page_data["index"]]:
+            break
+        offset = state.board.out_of_bounds_offset(
+          player.pieces[page_data["index"]].split(page_data["position"]))
+        page_data["position"] = (page_data["position"][0] - offset[0],
+                                 page_data["position"][1] - offset[1])
       elif key == keys.RIGHT:
-        if page_data["index"] < len(player.pieces) - 1:
-          page_data["index"] += 1
-        else:
-          page_data["index"] = 0
+        while True:
+          if page_data["index"] < len(player.pieces) - 1:
+            page_data["index"] += 1
+          else:
+            page_data["index"] = 0
+          if valid_moves[page_data["index"]]:
+            break
+        offset = state.board.out_of_bounds_offset(
+          player.pieces[page_data["index"]].split(page_data["position"]))
+        page_data["position"] = (page_data["position"][0] - offset[0],
+                                 page_data["position"][1] - offset[1])
       elif key == keys.ENTER:
         if state.board.validate(piece.split(page_data["position"]),
                                 player.color):
           state.place_piece(player, piece, page_data["position"])
-          goto("pieces", {"index": 0, "num_buffer": ""})
+          goto("pieces", {"index": 0, "num_buffer": ""}, ["any_valid_moves"])
+        else:
+          raise_alert("Piece is in an invalid position")
+      elif key == keys.ESCAPE:
+        goto("menu", {"page": page, "data": page_data, "select": 0})
       else:
         continue
       update_screen()
-    # elif command == "board":
-    #   goto("board")
-    # elif command == "pieces":
-    #   goto("pieces")
-    # elif command.startswith("piece "):
-    #   index = int(command[len("piece "):]) - 1
-    #   if index >= 0 and index < len(player.pieces):
-    #     goto("piece", {"piece": player.pieces[index]})
-    #   else:
-    #     raise_alert("Invalid piece index")
-    # elif page == "piece" and command.startswith("rotate "):
-    #   for c in command[len("rotate "):]:
-    #     if c == "+":
-    #       page_data["piece"].rotate_90()
-    #     elif c == "-":
-    #       page_data["piece"].rotate_neg_90()
-    # elif page == "piece" and command.startswith("place "):
-    #   if state.board.validate(
-    #       page_data["piece"].split(parse_position(command[len("piece "):])),
-    #       player.color):
-    #     goto(
-    #       "place_piece", {
-    #         "position": parse_position(command[len("piece "):]),
-    #         "piece": page_data["piece"]
-    #       })
-    #   else:
-    #     raise_alert("Piece cannot be placed in that location")
-    # elif page == "piece" and command.startswith("flip "):
-    #   dir = command[len("flip "):]
-    #   if dir == "horizontal" or dir == "h":
-    #     page_data["piece"].flip_horizontal()
-    #   elif dir == "vertical" or dir == "v":
-    #     page_data["piece"].flip_vertical()
-    #   else:
-    #     raise_alert("Piece cannot be flipped in that direction")
-    # elif page == "place_piece" and command.upper() == "Y":
-    #   if state.place_piece(player, page_data["piece"], page_data["position"]):
-    #     raise_alert("Piece was placed")
-    #     goto("board")
-    #   else:
-    #     raise_alert("Piece was not placed")
-    # elif page == "place_piece" and command.lower() == "n":
-    #   goto("piece", {"piece": page_data["piece"]})
-    # elif command == "pass":
-    #   state.pass_turn()
-    #   raise_alert("Player has passed")
-    #   goto("board")
-    # elif command == "help":
-    #   goto("help")
-    # elif command == "instructions":
-    #   goto("instructions")
-    # elif command == "exit":
-    #   break
-    # else:
-    #   raise_alert("Invalid command")
 
-    winner = state.check_win()
-    if winner is not None:
-      os.system(clear)
-      print(f"{winner.name} has won!")
-      state.board.print()
-      if input("Would you like to play again? (Y/n) ").upper() == "Y":
-        state = State()
-        page = "board"
-        page_data = {}
-        alerts = []
+    elif page == "menu":
+      actions = ["continue", "instructions", "help", "exit"]
+      if key == "w":
+        if page_data["select"] > 0:
+          page_data["select"] -= 1
+      elif key == "s":
+        if page_data["select"] < len(actions) - 1:
+          page_data["select"] += 1
+      elif key == keys.ENTER:
+        action = actions[page_data["select"]]
+        if action == "continue":
+          goto(page_data["page"], page_data["data"])
+        elif action == "exit":
+          break
+        else:
+          goto(
+            action, {
+              "page": page_data["page"],
+              "data": page_data["data"],
+              "select": page_data["select"]
+            })
+      elif key == keys.ESCAPE:
+        goto(page_data["page"], page_data["data"])
       else:
-        break
-  except:
-    raise_alert("Error has occurred")
-  finally:
-    time.sleep(1 / 20)
+        continue
+      update_screen()
+
+    elif page == "instructions" or page == "help":
+      if key == keys.ESCAPE:
+        goto(
+          "menu", {
+            "page": page_data["page"],
+            "data": page_data["data"],
+            "select": page_data["select"]
+          })
+        update_screen()
+
+  except Exception as e:
+    raise_alert(f"Error has occurred, {str(e)}")
